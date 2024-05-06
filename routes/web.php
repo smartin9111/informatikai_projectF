5a04b61a2eb966861d9833ae5ec78f10dd2e6e27
Sinka Martin <sinka.martin.91@gmail.com>
2024-05-06 21:17:26 +0200

    main, animation footer header mobile screen

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend/frontend_dashboard');
 });
 
 Route::get('/debug', [DebugController::class, 'index'])->name('debug');
